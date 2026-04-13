94ca426d201f4f46ec4fc7f8b265c9713b2912ab
Valtteri Timonen <vtimonen@hotmail.com>
2026-04-13 19:25:50 +0300

    create frontend service to send .jpg files to fastapi

--- lib/services/plantnet_service.dart
+++ lib/services/plantnet_service.dart
@@ -1,6 +1,11 @@
 // TODO: API-koodi
 import 'package:dio/dio.dart';
+import 'package:camera/camera.dart';
 
+
+// HUOM: Jos testaat fyysisellä puhelimella, vaihda 127.0.0.1 tilalle 
+// tietokoneesi lähiverkon IP-osoite (esim. 192.168.1.X) ja käynnistä 
+// backend komennolla: uvicorn main:app --host 0.0.0.0 --reload
 class PlantNetService {
   final Dio _dio = Dio(BaseOptions(
     baseUrl: 'http://127.0.0.1:8000/',
@@ -8,17 +13,20 @@ class PlantNetService {
     receiveTimeout: const Duration(seconds: 10),
   ));
 
-  Future<Map<String, dynamic>> identifyPlant(String imagePath) async {
+Future<Map<String, dynamic>?> identifyPlant(XFile imageFile) async {
     try {
       FormData formData = FormData.fromMap({
-        'image': await MultipartFile.fromFile(imagePath, filename: 'plant.jpg'),
+        'image': await MultipartFile.fromFile(
+          imageFile.path, 
+          filename: imageFile.name,
+        ),
       });
 
-      Response response = await _dio.post('/detect', data: formData);
+      Response response = await _dio.post('detect', data: formData);
       return response.data;
     } catch (e) {
-      print('Error identifying plant: $e');
-      return {};
+      print('Virhe PlantNetService-palvelussa: $e');
+      return null;
     }
   }
 }
